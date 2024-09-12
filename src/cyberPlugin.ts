6a0102f6b6a48476d3258c1d2ffa694c8f7fa122
Aaron <gabbert.aaron@gmail.com>
2024-09-12 09:00:59 -0500

    added CyberPlugin type

---
+++ src/cyberPlugin.ts
@@ -0,0 +1,14 @@
+import { Plugin } from "obsidian";
+import { searchSite } from "./sidebar";
+
+
+
+interface CyberPluginSettings {
+	validTld: string[];
+	searchSites: searchSite[];
+}
+
+export class CyberPlugin extends Plugin {
+	settings: CyberPluginSettings | undefined;
+	validTld: string[] | null | undefined;
+}
