2ae3bf1fd6c648d5564a5e8dfb1ad13d25128ce1
Aaron <gabbert.aaron@gmail.com>
2024-10-03 20:37:14 -0500

    added mock request function

--- __mocks__/obsidian.ts
+++ __mocks__/obsidian.ts
@@ -3,3 +3,7 @@ export const Plugin = jest.fn();
 export const SuggestModal = jest.fn();
 export const Modal = jest.fn();
 export const ItemView = jest.fn();
+export const request = jest.fn().mockReturnValue(`COM
+ORG
+ME
+INFO`);
