a102eed9c832d3cc7ff9af80b8805c397e17fb2d
Aaron <gabbert.aaron@gmail.com>
2024-10-16 09:09:40 -0500

    improved error notices

--- src/safeStorageProvider.ts
+++ src/safeStorageProvider.ts
@@ -1,4 +1,4 @@
-import { Platform } from "obsidian";
+import { Notice, Platform } from "obsidian";
 
 export { encryptString, decryptString };
 
@@ -10,30 +10,34 @@ if (Platform.isDesktop) {
     safeStorage = require("electron")?.remote?.safeStorage;
 }
 
-function encryptString(val: string): string | null {
+function encryptString(val: string): string {
     if (Platform.isDesktop && safeStorage && safeStorage.isEncryptionAvailable()) {
         try {
             const buff = safeStorage.encryptString(val) as Buffer;
             return buff.toString("base64");
         } catch(err) {
             console.error(`Failed to encrypt passed value. ${err}`);
-            return null;
+            new Notice("Failed to encrypt input string. Check console for error details.");
+            return "";
         }
     }
     console.error(`Safe storage provider not available.`);
-    return null;
+    new Notice("Failed to encrypt input string. Check console for error details.");
+    return "";
 }
 
-function decryptString(val: string): string | null {
+function decryptString(val: string): string {
     if (Platform.isDesktop && safeStorage && safeStorage.isEncryptionAvailable()) {
         try {
             const buff = Buffer.from(val, "base64");
             return safeStorage.decryptString(buff) as string;
         } catch(err) {
             console.error(`Failed to decrypt passed value. ${err}`);
-            return null;
+            new Notice("Failed to decrypt input string. Check console for error details.");
+            return "";
         }
     }
     console.error(`Safe storage provider not available.`);
-    return null;
+    new Notice("Failed to decrypt input string. Check console for error details.");
+    return "";
 }
